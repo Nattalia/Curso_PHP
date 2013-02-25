dcbdd8f24e0ae0b322d4a9aecb62931e533fcc1e
unknown <natt.81@gmail.com>
2013-02-25 21:52:39 +0100

    Formulario. Update de los usuarios

--- Formulario/public/formulario.php
+++ Formulario/public/formulario.php
@@ -1,52 +1,101 @@
-<?php 
-echo "<pre>";
+<?php
+echo "<pre>Post: ";
 print_r($_POST);
-echo "<pre/>";
+echo "</pre>";
 
-//echo "<hr/>";
+echo "<pre>Request: ";
+print_r($_REQUEST);
+echo "</pre>";
 
-echo "<pre>";
+require "functions.php";
+// Obtenemos los datos del usuario y lo guardamos en un array
+$file = "usuarios.txt";
+//$arrayFile = txtToArray($file);
+//$line = lineFromFile($file, $_GET["id"]);
+//$_GET=lineToArray($line);
+
+echo "<pre>Get: ";
 print_r($_GET);
-echo "<pre/>";
-?>
+echo "</pre>";
 
+?>
 <!DOCTYPE html>
-<html>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<body>	
-		<h1>Mi formulario</h1>		
-		<form action="procesa.php" method="post" enctype="multipart/form-data" >		
-			<div style="color:#0000FF">			
-				Id.: <input type="hidden" name="id" value="01"><br><br>
-				Nombre: <input type="text" name="name"><br><br>
-				Email: <input type="text" name="email"><br><br>
-				Contraseña: <input type="password" name="password"><br><br>
-				Dirección: <input type="text" name="direction"><br><br>
-				Descripción: <textarea name="description"></textarea><br><br>
-				Sexo: <br><input type="radio" name="sex" value="male">Hombre<br>
-					      <input type="radio" name="sex" value="female">Mujer<br>
-					      <input type="radio" name="sex" value="others">Otros<br><br>
-			    Ciudad: <select name="city">
-						  <option value="coruna">A Coruña</option>
-						  <option value="vigo">Vigo</option>
-						  <option value="pontevedra">Pontevedra</option>
-						  <option value="ourense">Ourense</option>
-						</select><br><br> 
-				Mascotas: <br><input type="checkbox" name="pets[]" value="perro">Perro<br>
-						  <input type="checkbox" name="pets[]" value="gato">Gato<br>
-						  <input type="checkbox" name="pets[]" value="caballo">Caballo<br><br>
-			    Deportes: <br><select multiple name="sports[]">
-								  <option value="futbol">Futbol</option>
-								  <option value="baloncesto">Baloncesto</option>
-								  <option value="balonmano">Balonmano</option>
-								  <option value="tenis">Tenis</option>
-							   </select><br><br>
-				Imagen: <input type="file" name="image"><br><br> 
-				
-				<input type="button" name="button" value="Button">
-				<input type="submit" name="submit" value="Submit" >
-				<input type="button" name="boton" value="Reset">					   		
-			</div> 		
-		</form>	
+<html lang="en">
+	<head>
+		<title>Formularios</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="robots" content="noarchive,noindex">
+		<meta name="description" content="Formulario">
+		<meta name="keywords" content="Formulario">
+	</head>
+	<body>
+		<div id="wrapper">		
+			<form action="procesar.php" method="POST" enctype="multipart/form-data">			
+					<div style="color:#0000FF">
+						Id.: <input type="hidden" name="id" value="01"><br><br>
+						Nombre: <input type="text" name="name"><br><br>
+						Email: <input type="text" name="email" ><br><br>
+						Contraseña: <input type="password" name="password" ><br><br>
+						Dirección: <input type="text" name="address" ><br><br>
+						Descripción: <textarea name="description"></textarea><br><br>
+						Sexo: <br>	<input type="radio" name="sex" value="male" >Hombre<br>
+									<input type="radio" name="sex" value="female">Mujer<br>
+									<input type="radio" name="sex" value="others">Otros<br><br>
+						Ciudad: <select name="city" >
+									<option value="coruna">A Coruña</option>
+									<option value="vigo">Vigo</option>
+									<option value="pontevedra">Pontevedra</option>
+									<option value="ourense">Ourense</option>
+								</select><br><br>
+						Mascotas: <br>	<input type="checkbox" name="pets[]" value="perro">Perro<br>
+										<input type="checkbox" name="pets[]" value="gato">Gato<br>
+										<input type="checkbox" name="pets[]" value="caballo">Caballo<br><br>
+						Deportes: <br>	<select multiple name="sports[]">
+											<option value="futbol">Futbol</option>
+											<option value="baloncesto">Baloncesto</option>
+											<option value="balonmano">Balonmano</option>
+											<option value="tenis">Tenis</option>
+										</select><br><br>
+						Imagen: <input type="file" name="photo"><br><br>
+						<input type="button" name="button" value="Button">
+						<input type="submit" name="submit" value="Submit" >
+						<input type="button" name="boton" value="Reset">
+						
+						<!--  
+						Id.: <input type="hidden" name="id" value="01"><br><br>
+						Nombre: <input type="text" name="name" value=<?=$_GET[1]?> ><br><br>
+						Email: <input type="text" name="email" value=<?=$_GET[2]?>><br><br>
+						Contraseña: <input type="password" name="password" value=<?=$_GET[3]?>><br><br>
+						Dirección: <input type="text" name="address" value=<?=htmlspecialchars($_GET[4])?>><br><br>
+						Descripción: <textarea name="description"><?=$_GET[5]?></textarea><br><br>
+						Sexo: <br>	<input type="radio" name="sex" value="male" >Hombre<br>
+									<input type="radio" name="sex" value="female">Mujer<br>
+									<input type="radio" name="sex" value="others">Otros<br><br>
+						Ciudad: <select name="city" >
+									<option value="coruna">A Coruña</option>
+									<option value="vigo">Vigo</option>
+									<option value="pontevedra">Pontevedra</option>
+									<option value="ourense">Ourense</option>
+								</select><br><br>
+						Mascotas: <br>	<input type="checkbox" name="pets[]" value="perro">Perro<br>
+										<input type="checkbox" name="pets[]" value="gato">Gato<br>
+										<input type="checkbox" name="pets[]" value="caballo">Caballo<br><br>
+						Deportes: <br>	<select multiple name="sports[]">
+											<option value="futbol">Futbol</option>
+											<option value="baloncesto">Baloncesto</option>
+											<option value="balonmano">Balonmano</option>
+											<option value="tenis">Tenis</option>
+										</select><br><br>
+						Imagen: <input type="file" name="photo"><br><br>
+						<input type="button" name="button" value="Button">
+						<input type="submit" name="submit" value="Submit" >
+						<input type="button" name="boton" value="Reset">
+						-->
+					</div> 
+			</form>				
+		</div>
+		
+		<div class="bottom">
+		</div>
 	</body>
-</html> 
+</html>
--- Formulario/public/functions.php
+++ Formulario/public/functions.php
@@ -1,135 +1,145 @@
 <?php
-/**
- * Functions that are used to generate the data
- * @author Natalia M. Rodriguez Rodriguez
- * @copyright 2013
- */
 
 /**
- * This function returns the string to write in the file
- * @param array $post that contains the post variables 
- * @param array $files that contains the files
- * @return string to write in the file
- */
- 
-function stringFormated($post, $files)
+* Shows an array (max two dimensions)
+* @param array $array 
+* @return true
+*/
+function muestraArray($array)
 {
-	$dataString="";
-	
-	foreach ($post as $key => $value)
-	{
-		if (strcmp($key, "submit") != 0)
+	foreach ($array as $key => $value)
+	{	
+		echo $key.": ";
+		if(is_array($value))
 		{
-			if (is_array($value))
-			{
-				foreach ($value as $value_2)
-				{					
-					if (next($value))						
-						$dataString .= $value_2.", ";
-					else						
-						$dataString .= $value_2;
-				}
-			}
-			else				
-				$dataString .= $value;
-				
-			$dataString .= " | ";
+			/* Aqui hay que poner algo */
+			// foreach ($value as $value2)
+			// echo $value2.",";
+			echo implode(',', $value);
 		}
+		else
+		{
+			echo $value;
+		}
+		echo "<br/>";
 	}
-	
-	foreach ($files as $value)
-	{
-		if (is_array($value))
-		{
-			foreach ($value as  $value_2)
-			{
-				if (next($value))					
-					$dataString .= $value_2.", ";
-				else					
-					$dataString .= $value_2;
-			}
-				
-		}
-	}	
-	
-	return $dataString."\n";
+
+	return TRUE;
 }
 
-/**
- * This function writes a string into a file
- * @param string $string string to write into the file
- * @param string $file name of the file
- * @return true is the string is correctly written, false in other case
- */
-
-function writeToFile($string, $file)
-{		
-	// Otra forma
-	
-	$fileWritten = TRUE;	
-	if (!$handler = fopen($file, 'a')) 
-	{
-		$fileWritten = FALSE;
-		echo "Error en fopen";
-		return $fileWritten;			
-	}
-
-	if (fwrite($handler, $string) === FALSE) 
-	{
-		$fileWritten = FALSE;
-		echo "Error en fwrite";
-		return $fileWritten;
-	}
-				
-	fclose($handler);
-		
-	return $fileWritten;
-}
 
 /**
- * This function uploads a file to a directory, renaming if exists
- * @param string $ruta path of the file
- * @param string $name name of the file 
- * @return TRUE
- */
-
-function uploadAndRenameFile($tmp_name, $ruta, $name)
-{	
-	if (file_exists($ruta."/".$name))
-	{	// si existe
-	// Buscar nombre
+* Upload a file to directory
+* @param array $arrayFiles
+* @return string $name
+*/
+function SubirArchivo($arrayFiles)
+{
+	$uploads_dir = "/uploads";
+	$tmp_name = $arrayFiles["photo"]["tmp_name"];
+	$name = $arrayFiles["photo"]["name"];
+	$ruta = $_SERVER['DOCUMENT_ROOT'].$uploads_dir;
+	$url = $uploads_dir;
 	
-	$a = 0;
-	$path_parts = pathinfo($ruta."/".$name);
-	// Mientras que Nombre-[contador].jqg exista en directorio, aumento contador
-	while (file_exists($ruta."/".$name))
+	// Comprobar si el nombre existe en el diretorio
+	if(file_exists($ruta."/".$name))
+	{	// Si existe BuscarNombre
+		$a=0;
+		$path_parts = pathinfo($ruta."/".$name);
+		// Mientras que Nombre-[contador].jpg EXISTA en directorio
+		while(file_exists($ruta."/".$name))
+		{
+			// Aumento contador
+			$a++;
+			// Cambiar el nombre y volver a intenter
+			$name=$path_parts['filename']."-".$a.".".$path_parts['extension'];
+		}
+		// Al salir tendre el contador que no existe
+		// Subir el fichero al directorio con el nuevo nombre
+		move_uploaded_file($tmp_name, $ruta."/".$name);
+	}
+	
+	else	// No existe
 	{
-		$a++;
-		// Cambiar el nombre y volver a intentar
-		$name = $path_parts['filename']."-".$a.".".$path_parts['extension'];
+		// Subir el fichero al directorio con el mismo nombre
+		move_uploaded_file($tmp_name, $ruta."/".$name);
 	}
-	// Al salir tendre el contador que no existe
-	// Subir el fichero al directorio con el Nombre-[contador].jpg
-	move_uploaded_file($tmp_name, $ruta."/".$name);
-	}
-	{	// no existe
-	// Subir el fichero al directorio con el mismo nombre
-	move_uploaded_file($tmp_name, $ruta."/".$name);
-	}	
+	
 	return $name;
 }
 
 
+/**
+* Recibe un array, de máximo dos dimensiones,
+* y devuelve un array con los elementos de la segunda
+* dimensión separados por comas.
+*
+* @param array array de entrada
+* @return array comma separated
+*/
 function cambiaArray($array)
 {
 	$array2 = array();
 	
-	foreach ($array as $key => $value){
-		if (is_array($value))
-			$array2[]=implode(',',$value);
-		else 
-			$array2[]=$value;		
+	foreach ($array as $key => $value)
+	{
+		if(is_array($value))
+			$array2[]=implode(',', $value);
+		else
+			$array2[]=$value;	
 	}
 	
 	return $array2;
 }
+
+/**
+ *  Function that returns an array whit all the lines of a txt file 
+ *
+ *  @param string $file file
+ *  @return array $array array
+ */
+function txtToArray($file)
+{
+	$content=file_get_contents($file);
+	$array=explode("\r",$content);
+	
+	return $array; 
+}
+
+
+/**
+ *  Function that returns a line from a file
+ *  
+ *  @param string $file file
+ *  @param integer $numLine line that we want 
+ *  @return string $line line 
+ */
+function lineFromFile($file, $numLine)
+{
+	$array = txtToArray($file);
+	$line = $array[$numLine];	
+	
+	return $line;
+}
+
+
+/**
+ *  Function that converts a string into an array 
+ *
+ *  @param string $line 
+ *  @return array $array
+ */
+function lineToArray($line){
+	$array=array();	
+	$array=explode("|", $line);
+	// Second level
+	foreach ($array as $key => $value){
+		if (strpos($value, ","))
+			$array[$key]=explode(",", $value);
+	}
+	return $array;
+}
+
+
+
+
