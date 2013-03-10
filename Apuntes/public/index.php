b539f333869e88cc0d6ec7620f6baa8f866a2da2
unknown <natt.81@gmail.com>
2013-03-10 20:30:07 +0100

    modificaciones wordpress

--- Apuntes/public/index.php
+++ Apuntes/public/index.php
@@ -1,37 +1,9 @@
 
 <?php
-function algunaFuncion() 
-{
-}
-
-$variableFuncion = 'algunaFuncion';
-
-var_dump(is_callable($variableFuncion, false, $nombre_a_llamar));  // bool(true)
-
-echo $nombre_a_llamar, "\n";  // algunaFuncion
 
 
 
-//
-//  Array que contiene un método
-//
+$time = strtotime ('2004/01/01');
 
-class algunaClase {
-
-	function algunMetodo()
-	{
-	}
-
-}
-
-
-$unObjeto = new algunaClase();
-
-$variableMetodo = array($unObjeto, 'algunMetodo');
-
-var_dump(is_callable($variableMetodo, true, $nombre_a_llamar));  //  bool(true)
-
-echo $nombre_a_llamar, "\n";  //  algunaClase::algunMetodo
-
-
+echo date ('H:\i:s', $time);
 ?>
